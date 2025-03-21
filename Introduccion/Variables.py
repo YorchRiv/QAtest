2790aa9c84c4c41d4d9bd30f40c94aae57f6e729
Jorge Mejicanos <jorgemejicanos145@gmail.com>
2025-03-20 20:33:45 -0600

    Introduccion

---
+++ Introduccion/Variables.py
@@ -0,0 +1,41 @@
+# Ejemplo de todos los tipos de variables en Python
+
+# Entero (int)
+numero_entero = 42
+print("Entero:", numero_entero)
+
+# Flotante (float)
+numero_flotante = 3.14159
+print("Flotante:", numero_flotante)
+
+# Cadena de texto (str)
+texto = "Hola, mundo"
+print("Cadena de texto:", texto)
+
+# Booleano (bool)
+es_verdadero = True
+print("Booleano:", es_verdadero)
+
+# Lista (list)
+lista = [1, 2, 3, 4, 5]
+print("Lista:", lista)
+
+# Tupla (tuple)
+tupla = (1, 2, 3, 4, 5)
+print("Tupla:", tupla)
+
+# Conjunto (set)
+conjunto = {1, 2, 3, 4, 5}
+print("Conjunto:", conjunto)
+
+# Diccionario (dict)
+diccionario = {"clave1": "valor1", "clave2": "valor2"}
+print("Diccionario:", diccionario)
+
+# Ningún valor (NoneType)
+sin_valor = None
+print("NoneType:", sin_valor)
+
+# Número complejo (complex)
+numero_complejo = 2 + 3j
+print("Número complejo:", numero_complejo)
